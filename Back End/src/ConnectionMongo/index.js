bc94b1d41e028119f7cf78c171a7a9d0659dd35f
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-11-08 17:32:44 -0300

    Feat: Realizando a configuração do mongoose

---
+++ Back End/src/ConnectionMongo/index.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+const dbURI = process.env.DB_URI || 'mongodb://localhost:27017/semantix';
+mongoose.connect(dbURI);
+
+
+const schema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  fornecedor: { type: String, required: false },
+  cozinheiro: { type: String, required: false },
+  suprimentos: { type: String, required: false },
+  arroz: { type: String, required: false },
+  feijao: { type: String, required: false },
+  macarrao: { type: String, required: false },
+})
+
+const model = mongoose.model('Semantix', schema);
+
+module.exports = model;
