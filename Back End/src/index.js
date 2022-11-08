b19ca9c70fb0e5651c771a2e57999ce0029a4c53
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-11-08 16:58:42 -0300

    Feat: inicializando o express/rota

---
+++ Back End/src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const PORT = 3001;
+const app = express();
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
