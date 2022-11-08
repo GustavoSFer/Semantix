eeaf573d1ca61552d970f34df26d3d85bd36ce6c
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-11-08 18:47:37 -0300

    Feat: Criando um hash para a senha

--- Back End/src/Service/providerService.js
+++ Back End/src/Service/providerService.js
@@ -1,10 +1,13 @@
 const model = require('../Model/providerModel');
+const md5 = require('md5');
 const schemaProvider = require('../Joi/schemaProvider');
 
 const createProvider = async (email, name, password, grupo, empresa, cnpj) => {
 
   await schemaProvider.validateAsync({ email, name, password, grupo, empresa, cnpj });
-  const providerAdd = await model.createProvider(email, name, password, grupo, empresa, cnpj);
+  const hashPassword = md5(password);
+
+  const providerAdd = await model.createProvider(email, name, hashPassword, grupo, empresa, cnpj);
 
   return providerAdd;
 };
