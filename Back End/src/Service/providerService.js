a7a9b83f0f8c7b382e47325d46c29509d2813652
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-11-08 18:44:45 -0300

    Feat: Verificando com o Joi as informações passadas

--- Back End/src/Service/providerService.js
+++ Back End/src/Service/providerService.js
@@ -1,6 +1,9 @@
 const model = require('../Model/providerModel');
+const schemaProvider = require('../Joi/schemaProvider');
 
 const createProvider = async (email, name, password, grupo, empresa, cnpj) => {
+
+  await schemaProvider.validateAsync({ email, name, password, grupo, empresa, cnpj });
   const providerAdd = await model.createProvider(email, name, password, grupo, empresa, cnpj);
 
   return providerAdd;
