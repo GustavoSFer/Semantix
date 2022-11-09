3b8d58672387cb92b6ebf73823d8b073a409cb3a
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-11-09 00:00:38 -0300

    Feat: Criando a primeira rota e verificando se esta rodando o app

---
+++ front-end/src/Pages/CreateUser.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CreateUser() {
+  return (
+    <div>
+      pagina de criar usuario;
+      <p>dsfsdfsdfs</p>
+    </div>
+  );
+}
+
+export default CreateUser;
