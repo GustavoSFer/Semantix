39ca3f0892bdbc3941bc4997c96211abbda48c5d
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-11-09 09:55:32 -0300

    Feat: criando o input para email e senha

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,8 +3,11 @@ import MyContext from './MyContext/MyContext';
 
 
 function App() {
+  const MIN_PASSWORD_LANGTH = 6;
 
-  const contextValue = { };
+  const contextValue = {
+    MIN_PASSWORD_LANGTH,
+  };
 
   return (
     <MyContext.Provider value={contextValue}>
--- front-end/src/Pages/Login.jsx
+++ front-end/src/Pages/Login.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Input from '../Components/Input';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <div>
-      pagina de criar usuario;
-      <p>dsfsdfsdfs</p>
-    </div>
+    <main className="container">
+      <div className="row row-cols-1 row-cols-lg-2">
+        <div className="col border border-success">
+          <img src="#" alt="logo da empresa" />
+          <h1>Faça seu Login na Plataforma</h1>
+        </div>
+        <div className="col border border-primary">
+          <Input
+            type="text"
+            name="E-mail"
+            handleChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+          <Input
+            type="password"
+            name="Senha"
+            handleChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+        </div>
+      </div>
+    </main>
   );
 }
 
