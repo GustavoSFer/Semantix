1c7ab9affba6de89c26a5220496d520bce7c0036
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-11-10 22:49:57 -0300

    Test: Model - buscando todos os dados

---
+++ Back End/src/Tests/Model/model.test.js
@@ -0,0 +1,16 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const model = require('../../ConnectionMongo');
+const { bodycreate, dataDb } = require('../Mock');
+const modelfind = require('../../Model');
+
+describe('Model', () => {
+  beforeEach(() => sinon.restore());
+  describe('FindAll', () => {
+    it('Quando tiver dados, deve retornar um array com as informações', async () => {
+      sinon.stub(model, 'find').resolves(dataDb);
+      const data = await modelfind.getAll();
+      expect(data).to.be.equal(dataDb);
+    });
+  });
+});
